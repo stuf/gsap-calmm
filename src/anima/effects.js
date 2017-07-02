61ed0efe2bffaf703413dd0212b64d0238990771
Stefan Rimaila <stefan@rimaila.fi>
2017-07-02 22:18:48 +0300

    Remove redundant effects

--- src/anima/effects.js
+++
@@ -1,5 +0,0 @@
-import * as U from 'karet.util';
-
-export const play = timeline => timeline.play();
-export const pause = timeline => timeline.pause();
-export const resume = timeline => timeline.resume();
