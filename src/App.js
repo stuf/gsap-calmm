177955193b4e1601739ad17a2af20d331d4632fd
Stefan Rimaila <stefan@rimaila.fi>
2017-07-02 21:43:33 +0300

    Update with comments

--- src/App.js
+++ src/App.js
@@ -3,23 +3,28 @@ import K, * as U from 'karet.util';
 import * as R from 'ramda';
 import { Elastic } from 'gsap';
 
+import './App.css';
 import { master as masterTimeline } from './common/timelines';
 import { LiftedUnits as Units } from './anima/lifted';
 import { Timeline as TL } from './anima/helpers';
-import './App.css';
 
-// Timeline
+import Sidebar from './components/sidebar';
+
+//
 
 const { events, timeline } = masterTimeline;
 const { pct } = Units;
 
-// Utilities
+// Utility functions
 
 const toFixed = (v, ds = 1) => v.toFixed(ds);
 const toPct = R.compose(toFixed, R.multiply(100));
 
 //
 
+/**
+ * Apply animation on the timeline, animating an element with a sequence of tweens.
+ */
 const applyAnimation = el =>
   U.seq(timeline,
         TL.from(el, 1.75, { left: Units.pct(10), ease: Elastic.easeOut }),
@@ -28,30 +33,41 @@ const applyAnimation = el =>
         TL.to(el, 1, { scale: 3, ease: Elastic.easeOut, delay: 1 }, '-=0.5'),
         TL.to(el, 1, { rotation: '+=180', ease: Elastic.easeOut }));
 
+/**
+ * Filter out anything that's _not_ an `onUpdate` event.
+ */
 const events$ = events.filter(R.whereEq({ type: 'onUpdate' }));
 
+/**
+ * Combine the `events$` stream we've created with the `timeline` stream
+ * to compute the progress as a percent value.
+ */
 const progress$ =
   U.seq(K(events$, timeline, (_, tl) => tl.progress()),
         U.lift1Shallow(toPct));
 
 //
 
-const App = ({ el = U.variable() }) =>
+const App = ({
+  /* Make `el` an atom without a value, which will hold the `ref` */
+  el = U.variable()
+}) =>
   <div className="app">
-    <section className="description">
-      <h2>Calmm and GreenSock</h2>
-
-      <p>
-        Wrapper for the <a href="https://greensock.com/">GreenSock animation platform</a> to make it possible to use the GreenSock libraries in <a href="https://github.com/calmm-js">Calmm</a> projects.
-      </p>
-    </section>
+    <Sidebar />
 
     <section className="animation-canvas">
       <div className="wrapper">
+        {/* `.smallbox` will be the element that we'll be animating
+            as an example. We're using `U.refTo` to set it as the value of `el` */}
         <div className="smallbox"
              ref={U.refTo(el)} />
+
+        {/* We're using a `sink` here, since we're not interested in
+            embedding the result into the VDOM, we're only applying
+            the animation on the element `el` */}
         {U.sink(applyAnimation(el))}
 
+        {/* Top-right controls */}
         <div className="controls">
           <div className="tl-progress">
             Timeline progress:
@@ -60,6 +76,8 @@ const App = ({ el = U.variable() }) =>
             </code>
           </div>
         </div>
+
+        {/* Progress bar */}
         <div className="tl-progressbar">
           <div className="indicator"
                style={{ left: pct(progress$) }} />
---
+++ src/components/sidebar.js
@@ -0,0 +1,12 @@
+import * as React from 'karet';
+
+const Sidebar = () =>
+  <aside className="description">
+    <h2>Calmm and GreenSock</h2>
+
+    <p>
+      Wrapper for the <a href="https://greensock.com/">GreenSock animation platform</a> to make it possible to use the GreenSock libraries in <a href="https://github.com/calmm-js">Calmm</a> projects.
+    </p>
+  </aside>;
+
+export default Sidebar;
